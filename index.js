46a178de2c2c86f8e0125c697c5aaa9d8f8c38ba
Tomas <silventetomas@gmail.com>
2024-05-13 18:47:06 -0300

    a

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'd99bc2d3dfmsh75e645227ee034fp12af27jsnbefcf1ac4534',
-		'X-RapidAPI-Host': 'ip-directory.p.rapidapi.com'
+		'X-RapidAPI-Host': 'ip-geolocation-ipwhois-io.p.rapidapi.com'
 	}
 };
-
 const fetchIpInfo = ip =>{
-    return fetch(`https://ip-directory.p.rapidapi.com/lookup/${ip}?risk=true&hostname=false`, options)
+    return fetch(`https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/?ip=${ip}`, options)
         .then(res => res.json())
         .catch(error => console.error(error))
 }
 
 const $ = selector => document.querySelector(selector)
 
-const form = document.querySelector('#form')
+const form = document.getElementById('form')
 const input = document.querySelector('#input')
 const submit = document.querySelector('#submit')
 const results = document.querySelector('#results')
 
-form.addEventListener('submit', async e =>{
-    e.preventDefault()
+form.addEventListener('submit', async (event) =>{
+    event.preventDefault()
     const {value} = input
     if(!value) return
 
@@ -28,8 +27,11 @@ form.addEventListener('submit', async e =>{
 
     const info = await fetchIpInfo(value)
 
+    const {ip, city, country, org, latitude, longitude} = info
+
     if(info){
-        results.innerHTML = JSON.stringify(info, null, 2)
+        results.innerHTML = ([`IP: ${ip} <br /> CITY: ${city} <br /> COUNTRY: ${country} <br /> ORG: ${org} <br /> LATITUDES: ${latitude} <br /> LONGITUDE: ${longitude}`])
+        // results.innerHTML = JSON.stringify(info, null,2)
     }
 
     submit.removeAttribute('disabled')
